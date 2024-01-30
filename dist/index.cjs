7f7e165d1cb54455f6bec03a5c751ea602d57f8e
Matko Klaic <matko.klaic@gmail.com>
2024-01-30 15:14:41 +0100

    feat!: Parametrise generateUrl method with baseUrl
        BREAKING CHANGE: generateUrl should be used with additional parameter baseUrl.

--- dist/index.cjs
+++ dist/index.cjs
@@ -1,10 +1,17 @@
 'use strict';
 
-const generateUrl = (params) => {
-  const baseUrl = "http://testurl.bitfinx.com/";
+const generateUrl = (baseUrl, params) => {
   let url = new URL(baseUrl);
 
-  for (const [key, value] of Object.entries(params)) {
+  for (const [key, value] of Object.entries(params).sort((a, b) => {
+    if (a[0] < b[0]) {
+      return -1;
+    }
+    if (a[0] > b[0]) {
+      return 1;
+    }
+    return 0;
+  })) {
     if (value !== undefined && value !== null && value !== "") {
       url.searchParams.set(key, encodeURIComponent(value));
     }
